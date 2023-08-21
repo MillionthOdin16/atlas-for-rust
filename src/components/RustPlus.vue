6a396003ab9d9723458862f20e8c9646f02626d2
MillionthOdin16 <102247808+MillionthOdin16@users.noreply.github.com>
2023-08-21 14:32:38 -0400

    Add patrol heli map asset

--- src/components/RustPlus.vue
+++ src/components/RustPlus.vue
@@ -209,8 +209,7 @@
           <l-tooltip content="Patrol Helicopter"/>
           <l-icon :icon-size="[20, 20]">
             <div style="position:relative" :style="{ transform: 'rotate('+ (-mapMarker.rotation) +'deg)'}">
-              <img src="images/map/chinook_map_body.png" width="30" height="30" style="width:30px;height:30px;"/>
-              <img src="images/map/chinook_map_blades.png" width="20" height="20" class="chinook-blade-spin-anticlockwise" style="position: absolute;top:-5px;left:5px;width:20px;height:20px;"/> <!-- anti clockwise rotation -->
+              <img src="images/map/patrol_heli_map_body.png" width="30" height="30" style="width:30px;height:30px;"/>
               <img src="images/map/chinook_map_blades.png" width="20" height="20" class="chinook-blade-spin-clockwise" style="position: absolute;top:15px;left:5px;width:20px;height:20px;"/> <!-- clockwise rotation -->
             </div>
           </l-icon>
