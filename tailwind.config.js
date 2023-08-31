09cda09489d276077e4adce98098f71dfea0d8de
MillionthOdin16 <102247808+MillionthOdin16@users.noreply.github.com>
2023-08-31 13:05:45 -0400

    Additional colors for map icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,28 @@ module.exports = {
         'rust-team-member-online': '#A6EA33',
         'rust-team-member-offline': '#686B6B',
         'rust-team-member-dead': '#CD412B',
+        lime: {
+          100: '#F0FCCF',
+          200: '#DCF9A3',
+          300: '#C8F477',
+          400: '#B1F051',
+          500: '#94CC16',
+          600: '#75A10E',
+          700: '#5C7D0A',
+          800: '#3E5C07',
+          900: '#244105',
+        },
+        cyan: {
+          100: '#CFFAFE',
+          200: '#A5F3FC',
+          300: '#67E8F9',
+          400: '#22D3EE',
+          500: '#06B6D4',
+          600: '#0891B2',
+          700: '#0E7490',
+          800: '#155E75',
+          900: '#164E63',
+        },
       }
     },
     zIndex: {
